cef078804a6fc9907cc83e8e14b291183c6b38d6
hossein <Hossein.m.7782@gmail.com>
2023-11-10 12:13:21 +0330

    add welcome message

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,2 +1,10 @@
 import os
 os.system('cls')
+
+def print_welcome():
+    print('Welcome to this game.')
+    print('Thanks for choice us. ')
+    print('Please start the game.')
+    print('-'*35)
+
+print_welcome()
